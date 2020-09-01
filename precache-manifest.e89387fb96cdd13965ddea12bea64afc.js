95325a0ab6eaaf0142a4201559e32154311caa85
sudo-corvus <neo11prince@gmail.com>
2020-09-02 02:05:10 +0530

    Updates

---
+++ precache-manifest.e89387fb96cdd13965ddea12bea64afc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c8f0ef28875983322c37a3155f2aba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "596a05053f77267ab4ac",
+    "url": "/portfolio/static/css/main.ecffbbc3.chunk.css"
+  },
+  {
+    "revision": "86bf982b244231e70318",
+    "url": "/portfolio/static/js/2.78e77c3e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.78e77c3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "596a05053f77267ab4ac",
+    "url": "/portfolio/static/js/main.d386f3e3.chunk.js"
+  },
+  {
+    "revision": "ad416dac48bf5e4beb91",
+    "url": "/portfolio/static/js/runtime-main.51a95c5c.js"
+  }
+]);
